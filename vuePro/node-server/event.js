0bb2b2b5db55bd1d96c2d37ef7917636bc55f87c
wlsh123 <975135062@qq.com>
2020-09-08 14:05:28 +0800

    node事件触发机制

--- vuePro/node-server/event.js
+++ vuePro/node-server/event.js
@@ -1,3 +1,4 @@
+/* 文档写法
 var EventEmitter = require("events").EventEmitter;
 var event = new EventEmitter();
 event.on("some_event", function () {
@@ -6,3 +7,17 @@ event.on("some_event", function () {
 setTimeout(function () {
   event.emit("some_event");
 }, 1000);
+*/
+
+//写法二：node事件触发机制
+//1. 导入events模块
+var event = require("events");
+//2. 创建eventsEmitter对象
+var EventEmitter = new event.EventEmitter();
+//3. 绑定事件
+EventEmitter.on("connection", function () {
+  console.log("绑定connection事件完成。");
+});
+//4. 触发事件
+EventEmitter.emit("connection");
+console.log("程序执行完毕/...");
